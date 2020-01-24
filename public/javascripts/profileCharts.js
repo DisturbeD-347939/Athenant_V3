285c65cf79157162a86bb975bbaacbfbbb4ee5e7
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-01-24 11:12:26 +0000

    Fixed graph bug

--- public/javascripts/profileCharts.js
+++ public/javascripts/profileCharts.js
@@ -1,36 +1,51 @@
-var year = new Date().getFullYear();
-var changedYear = 1;
+var currentYear = new Date().getFullYear();
+var monthYear = new Date().getFullYear();
+var weekYear = new Date().getFullYear();
 
-setTimeout(function()
-{
-    $('#changeDate').text(year-1);
-}, 20)
+var chartWeeks;
+var chartMonths;
 
+var parsedTime;
 
 function setupCharts(percentile, times)
 {
+    console.log("Setting up charts");
     percentile = percentile.split(",");
     personalityChart(percentile);
-    displayTimes(times);
+    parseTimes(times, function(data){ parsedTime = data });
+    displayTimesWeek(0);
+    displayTimesMonth(0);
 }
 
 function changeDate(data)
 {
-    if(changedYear == 0)
+    if(data.graph == "month")
     {
-        year = new Date().getFullYear();
-        $('#changeDate').text(year);
-        changedYear++;
+        if(data.direction == "back")
+        {
+            monthYear--;
+        }
+        else
+        {
+            monthYear++;
+        }
+        displayTimesMonth(1);
     }
     else
     {
-        year--;
-        $('#changeDate').text(year-1);
-        changedYear--;
+        $('#timesWeekdaysChart').remove();
+        $('#weekChart > .chartjs-size-monitor').remove();
+        $("<canvas id='timesWeekdaysChart'>").insertAfter("#weekChart > img");
+        if(data.direction == "back")
+        {
+            weekYear--;
+        }
+        else
+        {
+            weekYear++;
+        }
+        displayTimesWeek(1);
     }
-    console.log(year + " | " + changedYear);
-    $('#changeDate').text(year);
-    displayTimes(data.times);
 }
 
 function personalityChart(data)
@@ -44,7 +59,6 @@ function personalityChart(data)
 
     for(var i = 0; i < big5.length; i++)
     {
-        console.log("here");
         var singleTrait = big5[i].replace(" ", "");
         $('#big5 > div').append("<p>" + big5[i] + "</p><div class='backgroundProgressBar'><div id='" + singleTrait + "progressBar'><p id='" + singleTrait + "percentile'></p></div></div>");
         $('#' + singleTrait + 'progressBar').width(data[i]*100 + '%');
@@ -52,45 +66,41 @@ function personalityChart(data)
     }
 }
 
-function displayTimes(times)
+function displayTimesWeek(update)
 {
-    parseTimes(times, function(data)
+    times = parsedTime;
+    //TIMES PER WEEKDAY
+    var weekdays = [0,0,0,0,0,0,0];
+    for(var i = 0; i < times.length; i++)
     {
-        times = data;
+        if(times[i][0] == "Mon" && times[i][5] == weekYear) weekdays[0]++;
+        else if(times[i][0] == "Tue" && times[i][5] == weekYear) weekdays[1]++;
+        else if(times[i][0] == "Wed" && times[i][5] == weekYear) weekdays[2]++;
+        else if(times[i][0] == "Thu" && times[i][5] == weekYear) weekdays[3]++;
+        else if(times[i][0] == "Fri" && times[i][5] == weekYear) weekdays[4]++;
+        else if(times[i][0] == "Sat" && times[i][5] == weekYear) weekdays[5]++;
+        else if(times[i][0] == "Sun" && times[i][5] == weekYear) weekdays[6]++;
+    }
 
-        //TIMES PER MONTH
-        var monthCount = [0,0,0,0,0,0,0,0,0,0,0,0];
-        for(var i = 0; i < times.length; i++)
+    var chart = document.getElementById('timesWeekdaysChart');
+
+    if(!update)
+    {
+        chartWeeks = new Chart(chart,
         {
-            if(times[i][1] == "Jan" && times[i][5] == year) monthCount[0]++;
-            else if(times[i][1] == "Feb" && times[i][5] == year) monthCount[1]++;
-            else if(times[i][1] == "Mar" && times[i][5] == year) monthCount[2]++;
-            else if(times[i][1] == "Apr" && times[i][5] == year) monthCount[3]++;
-            else if(times[i][1] == "May" && times[i][5] == year) monthCount[4]++;
-            else if(times[i][1] == "Jun" && times[i][5] == year) monthCount[5]++;
-            else if(times[i][1] == "Jul" && times[i][5] == year) monthCount[6]++;
-            else if(times[i][1] == "Aug" && times[i][5] == year) monthCount[7]++;
-            else if(times[i][1] == "Sep" && times[i][5] == year) monthCount[8]++;
-            else if(times[i][1] == "Oct" && times[i][5] == year) monthCount[9]++;
-            else if(times[i][1] == "Nov" && times[i][5] == year) monthCount[10]++;
-            else if(times[i][1] == "Dec" && times[i][5] == year) monthCount[11]++;
-        }
-
-        var chart = document.getElementById('timesMonthChart');
-        new Chart(chart,
-	    {
-	    	type: 'line',
+            type: 'bar',
             data: 
             {
-	    		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',],
+                labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
                 datasets: 
                 [{
-	    			fill: false,
-	    			borderColor: 'red',
-	    			backgroundColor: 'red',
-	    			data: monthCount
-	    		}]
-	    	},
+                    label: "Tweets per weekday in " + weekYear,
+                    fill: false,
+                    borderColor: 'black',
+                    backgroundColor: '#E7DFC6',
+                    data: weekdays
+                }]
+            },
             options: 
             {
                 scales: 
@@ -101,40 +111,71 @@ function displayTimes(times)
                         {
                             beginAtZero: true,
                             precision:0
+                        },
+                        gridLines: 
+                        {
+                            color: "rgba(0, 0, 0, 0)",
                         }
-                    }]
+                    }],
+                    xAxes:
+                    {
+                        gridLines: 
+                        {
+                            color: "rgba(0, 0, 0, 0)",
+                        }
+                    }
                 }
-	    	}
+            }
         });
-        
-        //TIMES PER WEEKDAY
-        var weekdays = [0,0,0,0,0,0,0];
-        for(var i = 0; i < times.length; i++)
-        {
-            if(times[i][0] == "Mon" && times[i][5] == year) weekdays[0]++;
-            else if(times[i][0] == "Tue" && times[i][5] == year) weekdays[1]++;
-            else if(times[i][0] == "Wed" && times[i][5] == year) weekdays[2]++;
-            else if(times[i][0] == "Thu" && times[i][5] == year) weekdays[3]++;
-            else if(times[i][0] == "Fri" && times[i][5] == year) weekdays[4]++;
-            else if(times[i][0] == "Sat" && times[i][5] == year) weekdays[5]++;
-            else if(times[i][0] == "Sun" && times[i][5] == year) weekdays[6]++;
-        }
+    }
+    else
+    {
+        chartWeeks.data.datasets[0].label = "Tweets per month in " + weekYear;
+        chartWeeks.data.datasets[0].data = weekdays;
+        chartWeeks.update();
+    }
+}
 
-        var chart = document.getElementById('timesWeekdaysChart');
-        new Chart(chart,
-	    {
-	    	type: 'bar',
+function displayTimesMonth(update)
+{
+    times = parsedTime;
+    //TIMES PER MONTH
+    var monthCount = [0,0,0,0,0,0,0,0,0,0,0,0];
+    for(var i = 0; i < times.length; i++)
+    {
+        if(times[i][1] == "Jan" && times[i][5] == monthYear) monthCount[0]++;
+        else if(times[i][1] == "Feb" && times[i][5] == monthYear) monthCount[1]++;
+        else if(times[i][1] == "Mar" && times[i][5] == monthYear) monthCount[2]++;
+        else if(times[i][1] == "Apr" && times[i][5] == monthYear) monthCount[3]++;
+        else if(times[i][1] == "May" && times[i][5] == monthYear) monthCount[4]++;
+        else if(times[i][1] == "Jun" && times[i][5] == monthYear) monthCount[5]++;
+        else if(times[i][1] == "Jul" && times[i][5] == monthYear) monthCount[6]++;
+        else if(times[i][1] == "Aug" && times[i][5] == monthYear) monthCount[7]++;
+        else if(times[i][1] == "Sep" && times[i][5] == monthYear) monthCount[8]++;
+        else if(times[i][1] == "Oct" && times[i][5] == monthYear) monthCount[9]++;
+        else if(times[i][1] == "Nov" && times[i][5] == monthYear) monthCount[10]++;
+        else if(times[i][1] == "Dec" && times[i][5] == monthYear) monthCount[11]++;
+    }
+
+    var chart = document.getElementById('timesMonthChart');
+
+    if(!update)
+    {
+        chartMonths = new Chart(chart,
+        {
+            type: 'line',
             data: 
             {
-	    		labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',],
                 datasets: 
                 [{
-	    			fill: false,
-	    			borderColor: 'red',
-	    			backgroundColor: 'red',
-	    			data: weekdays
-	    		}]
-	    	},
+                    label: "Tweets per month in " + monthYear,
+                    fill: false,
+                    borderColor: '#2274A5',
+                    backgroundColor: '131B23',
+                    data: monthCount
+                }]
+            },
             options: 
             {
                 scales: 
@@ -145,12 +186,29 @@ function displayTimes(times)
                         {
                             beginAtZero: true,
                             precision:0
+                        },
+                        gridLines: 
+                        {
+                            color: "rgba(0, 0, 0, 0)",
                         }
-                    }]
+                    }],
+                    xAxes:
+                    {
+                        gridLines: 
+                        {
+                            color: "rgba(0, 0, 0, 0)",
+                        }
+                    }
                 }
-	    	}
+            }
         });
-    })
+    }
+    else
+    {
+        chartMonths.data.datasets[0].label = "Tweets per month in " + monthYear;
+        chartMonths.data.datasets[0].data = monthCount;
+        chartMonths.update();
+    }
 }
 
 function parseTimes(times, callback)
