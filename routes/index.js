9edadabf9bcff796ada1adc627212815b4d2eef3
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-01-24 11:16:30 +0000

    Remove unused code

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,4 @@ router.get('/', function(req, res, next)
   res.render('index', { title: 'Athenant' , error: 0});
 });
 
-/*router.post('/result', function(req, res, next)
-{
-    console.log(res.body);
-    twitter.getData(req.body, function(percentile, times)
-    {
-        res.render('results', {percentile: percentile, times: times});
-    });
-});*/
-
 module.exports = router;
