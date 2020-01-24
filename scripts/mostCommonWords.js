e67ee26e60e7d33b13ce2325703ade0b9eef9de2
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-01-24 11:20:34 +0000

    Added new lists of words to remove

--- scripts/mostCommonWords.js
+++ scripts/mostCommonWords.js
@@ -1,11 +1,13 @@
 var common = require('common-words');
 var mostCommon = require('most-common');
+sw = require('stopword')
 
+var moreCommon = ['i\'m', 'and', 'don\'t', 'is', 'you', '-', 'i\'ve', 'i', 'it\'s', 'can\'t', 'that\'s', '\'cause', 'you\'re', 'i\'ll'];
 module.exports = 
 {
     getMostCommonWords: function (id, callback)
     {
-        var profileData = require('../users/' + req.query.watsonID + '/profile.json');
+        var profileData = require('../users/' + id + '/profile.json');
         var content = "";
         for(var i = 0; i < profileData['contentItems'].length; i++)
         {
@@ -13,20 +15,38 @@ module.exports =
             if((i+1) >= profileData['contentItems'].length)
             {
                 content = content.split(" ");
+                content = sw.removeStopwords(content);
                 for(var k = 0; k < content.length; k++)
                 {
-                    content[k] = content[k].replace(/\\/, "");
+                    content[k] = content[k].toLowerCase();
+                    for(var j = 0; j < content[k].length; j++)
+                    {
+                        if(content[k][j] == '\’')
+                        {
+                            content[k] = content[k].replace("\’", "'");
+                        }
+                    }
+                    for(var j = 0; j < moreCommon.length; j++)
+                    {
+                        if(content[k] == moreCommon[j] || content[k][0] == "@")
+                        {
+                            content.splice(k, 1);
+                        }
+                    }
                     if((k+1) >= content.length)
                     {
                         content = removeCommonWords(content, common);
+                        content = mostCommon(content, 50);
+                        callback(content);
                     }
+                
                 }
             }
         }
     }
 }
 
-function removeCommonWords(words, common) 
+function removeCommonWords(words) 
 {
     common.forEach(function(obj) 
     {
