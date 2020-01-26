eef720aeff02d8dda4f851c8f702899475983cec
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-01-26 13:33:49 +0000

    Added a formatting function

--- scripts/main.js
+++ scripts/main.js
@@ -9,5 +9,32 @@ module.exports =
         var bitmap = fs.readFileSync(file);
         // convert binary data to base64 encoded string
         return "data:image/png;base64," + Buffer.from(bitmap).toString('base64');
+    },
+
+    formatNumber: function(number)
+    {
+        number = number.toString();
+        var formatted = "";
+        if(number.length > 6)
+        {
+            for(var i = 0; i < (number.length - 6); i++)
+            {
+                formatted += number[i]; 
+            }
+            formatted += "." + number[number.length - 6] + "M";
+        }
+        else if(number.length > 3)
+        {
+            for(var i = 0; i < (number.length - 3); i++)
+            {
+                formatted += number[i]; 
+            }
+            formatted += "." + number[number.length - 3] + "k";
+        }
+        else
+        {
+            return number;
+        }
+        return formatted;
     }
 }
