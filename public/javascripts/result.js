8da5e6c938877662e6a9989f8c81292c19db38fb
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-01-26 13:19:42 +0000

    Removed unnecessary function

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -183,20 +183,6 @@ function setup()
     
 }
 
-//Show further information
-function big5Click(event, array)
-{
-    splitPercentile = percentile.split(",");
-    for(var i = 0; i < splitPercentile.length; i++)
-    {
-        splitPercentile[i] = Math.round(splitPercentile[i]*100)/100;
-    }
-    if(typeof splitPercentile[array[0]._index !== 'undefined'])
-    {
-        console.log(splitPercentile[array[0]._index]);
-    }
-}
-
 function getInfo(callback)
 {
     $.ajax
